ab603c7efd90c8a575f0a26f60379f017171553c
JohnLoyal1337 <johnloyal1337@yandex.ru>
2026-07-10 16:24:57 +0500

    Api

--- frontend/app.js
+++ frontend/app.js
@@ -19,7 +19,7 @@ async function handleLogin() {
     const tabNum = document.getElementById("login-tab").value;
     const password = document.getElementById("login-pass").value;
     try {
-        const response = await fetch(`${API_URL}/auth/login`, {
+        const response = await fetch(`${API_URL}api/auth/login`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ tab_num: tabNum, password: password })
